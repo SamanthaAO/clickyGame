5cd8f5ffbd968dd467436c49d506d4badea3e4cd
SamanthaAO <samanthaaorcutt@gmail.com>
2019-08-06 17:47:46 -0700

    changed size of font for mobile responsiveness

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -6,7 +6,7 @@ const JumbotronPage = () => {
   return (
           <MDBJumbotron className="myJumbotron w-100 h-50 text-center">
                 <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold text headingText">The Many (rediculous) Faces of BoJo</MDBCardTitle>
-                <p className="mx-5 mb-5 text mainText">Click on an image to earn points, but don't click on any more than once!</p>
+                <p className="mx-3 mb-5 text mainText">Click on an image to earn points, but don't click on any more than once!</p>
           </MDBJumbotron>
   )
 }
